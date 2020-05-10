f3269bb11b05598b783186c75ce83a6fc7298adb
Colin Campbell <colincampbell321123@hotmail.com>
2020-05-10 20:05:37 +0100

    Add external dependency

--- hello.go
+++ hello.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"example.com/user/hello/morestrings"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println("Hello world")
 
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
